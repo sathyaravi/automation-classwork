883ce99ce96c7707a3cda2254211ccacc249fdaa
sathyaravi <sathyaravichandran92@gmail.com>
2025-10-05 22:30:12 -0700

    Added New program files

---
+++ MavenProject/src/main/java/week1/day1/Firefox.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		
+		double browserVersion = 100.2;
+		String browserName= "Firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		System.out.println("BrowserVersion is:"+browserVersion);
+		System.out.println("Browser Name is:"+browserName);
+		System.out.println("Visibility:"+isVisible);
+		System.out.println("Release Year:"+releaseYear);
+		System.out.println("Browser Logo:"+browserLogo);
+		
+
+	
+	}
+
+}
---
+++ MavenProject/src/main/java/week2day1/ConditionalStatementsIfElse.java
@@ -0,0 +1,27 @@
+package week2day1;
+
+public class ConditionalStatementsIfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int score = 67;
+		if (score >= 90) {
+			System.out.println("Grade S");
+		}
+		else if (score >= 80) {
+			System.out.println("Grade A");
+		}
+		else if (score >= 70) {
+			System.out.println("Grade B");
+		}
+		else if (score >= 60) {
+			System.out.println("Grade C");
+		} 
+		else {
+			System.out.println("Grade D");
+		}
+
+
+	}
+
+}
